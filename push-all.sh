573a705aefa19c4ecc5859ae02fc10d9db299966
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-07-10 17:30:54 +1200

    Oops: fix latest texlive tag

--- push-all.sh
+++ push-all.sh
@@ -8,15 +8,14 @@ source util.sh
 
 # Add short tags if possible
 trace docker tag "mjenz/texlive:$LATEST_UBUNTU" "mjenz/texlive:latest" || true
-trace docker tag "mjenz/texlive:$DISTRO-vanilla-$TEXLIVE_VERSION" "mjenz/texlive:$DISTRO-vanilla" || true
+trace docker tag "mjenz/texlive:$DISTRO-vanilla-$LATEST_TEXLIVE" "mjenz/texlive:$DISTRO-vanilla" || true
 trace docker tag "mjenz/texlive:$LATEST_UBUNTU-vanilla" "mjenz/texlive:vanilla" || true
 trace docker images mjenz/texlive
 
 # Push every image
 for i in $(docker images mjenz/texlive --format "{{.Repository}}:{{.Tag}}"); do
     if [[ $i != *"<none>" ]]; then
-        # Somehow Docker Hub ends up with mjenz/texlive:<none> and then explodes
-        # if you touch it
+        # Somehow this ends up with mjenz/texlive:<none> which is un-pushable
         trace docker push "$i"
     fi
 done
